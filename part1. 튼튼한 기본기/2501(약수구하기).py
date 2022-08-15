8f323d4b1d9f3b6b8ca61542fff0396308682d18
조기범 <akfmzh1207@naver.com>
2022-08-15 17:03:57 +0900

    BOJ

---
+++ part1. 튼튼한 기본기/2501(약수구하기).py
@@ -0,0 +1,16 @@
+N,K = map(int,input().split())
+
+ANSWER = []
+for i in range(1,N+1):
+    
+    if N%i==0:
+        ANSWER.append(i)
+
+    
+    
+if len(ANSWER) < K:
+    print(0)
+
+else:
+    print(ANSWER[K-1])
+
