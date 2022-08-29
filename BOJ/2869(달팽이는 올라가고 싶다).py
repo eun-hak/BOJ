45cbbf91abfae1a14aff8844af1875cb2756cdd5
조기범 <akfmzh1207@naver.com>
2022-08-30 00:14:08 +0900

    Update 2869(달팽이는 올라가고 싶다).py

--- BOJ/2869(달팽이는 올라가고 싶다).py
+++ BOJ/2869(달팽이는 올라가고 싶다).py
@@ -1,7 +1,5 @@
 a,b,c = map(int,input().split())
 
-
-
 k= (c-b)/(a-b)
     
 if k==int(k):
@@ -9,21 +7,17 @@ if k==int(k):
 else:
     print(int(k)+1)
 
-
-
-
-
 '''
-day=0
+date=0
 l=0
 while(True):
-    day+=1
+    date+=1
     l+=a
     if l>=c:
         break
     l-=b
 
-print(day)
+print(date)
 '''
     
 
