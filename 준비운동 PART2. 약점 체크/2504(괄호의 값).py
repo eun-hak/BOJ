cb68e81578f6b9bec6fe560a2fd5891f7be68c95
조기범 <akfmzh1207@naver.com>
2023-03-20 14:21:02 +0900

    3/20 스터디

---
+++ 준비운동 PART2. 약점 체크/2504(괄호의 값).py
@@ -0,0 +1,48 @@
+array = list(input())
+
+
+stack = []
+answer = 0
+tmp = 1
+
+for i in range(len(array)):
+    if array[i] == "(":
+        stack.append(array[i])
+        tmp*=2
+        
+    
+    elif array[i]  =="[":
+        stack.append(array[i])
+        tmp*=3
+        
+
+        
+    elif array[i]  ==")":
+        if not stack or stack[-1] == "[":       #[)를 의미함
+            answer = 0
+            break
+        if array[i-1] =="(":
+          
+            answer+=tmp
+        stack.pop()
+        tmp//=2
+
+    else:
+        if not stack or stack[-1] == "(":           #(]를 의미함
+            answer = 0
+            break
+        if array[i-1] == "[":
+           
+            answer+=tmp
+        stack.pop()
+        tmp//=3
+
+if stack:
+    print(0)
+else:
+    print(answer)
+#괄호가 연속되면 tmp로 계속 곱하게됨
+#누적 계산임 [()[]] 이걸 예시로두면 [() 이걸 계산할 때 이미 곱하기가 되어있음
+#하넙에 곱하는게 아니고 순차적으로 곱하는 방식
+
+#[()[]]
